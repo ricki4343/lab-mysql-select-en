8c9b9dbdc617445ac65b6fb5e0e8e275cb881527
Ricki Ahadian <rickiahadian@gmail.com>
2026-02-10 00:43:01 +0100

    Ricki Ahadian Week4 Lab3

--- data/solutions.sql
+++ data/solutions.sql
@@ -1,73 +1,88 @@
-USE pubs;
+USE sakila;
 
-/*Challenge 1*/
-SELECT 
-	authors.au_id AS 'AUTHOR ID', 
-    authors.au_lname AS 'LAST NAME', 
-    authors.au_fname AS 'FIRST NAME', 
-    titles.title AS 'TITLE', 
-    publishers.pub_name AS 'PUBLISHER'
-FROM authors
-JOIN titleauthor
-	ON authors.au_id = titleauthor.au_id
-JOIN  titles
-	ON titleauthor.title_id = titles.title_id
-JOIN publishers
-	ON titles.pub_id = publishers.pub_id
-ORDER BY authors.au_id;
+/*Query 1*/
+SELECT film.title AS 'Movie Title', inventory.store_id AS 'Store', COUNT(film.film_id) AS 'Copies'
+FROM film
+LEFT JOIN inventory
+	ON film.film_id = inventory.film_id
+WHERE film.title = 'Hunchback Impossible'
+GROUP BY film.title, inventory.store_id;
 
 
-/*Challenge 2*/
-SELECT 
-	authors.au_id AS 'AUTHOR ID', 
-    authors.au_lname AS 'LAST NAME', 
-    authors.au_fname AS 'FIRST NAME',  
-    publishers.pub_name AS 'PUBLISHER',
-    COUNT(titles.title) AS 'TITLE COUNT'
-FROM authors
-JOIN titleauthor
-	ON authors.au_id = titleauthor.au_id
-JOIN  titles
-	ON titleauthor.title_id = titles.title_id
-JOIN publishers
-	ON titles.pub_id = publishers.pub_id
-GROUP BY authors.au_id, authors.au_lname, authors.au_fname, publishers.pub_name
-ORDER BY COUNT(titles.title) DESC, authors.au_id DESC;
-
-/*Challenge 3*/
-SELECT 
-	authors.au_id AS 'AUTHOR ID', 
-    authors.au_lname AS 'LAST NAME', 
-    authors.au_fname AS 'FIRST NAME', 
-	SUM(sales.qty) AS 'TOTAL'
-FROM authors
-JOIN titleauthor
-	ON authors.au_id = titleauthor.au_id
-JOIN  titles
-	ON titleauthor.title_id = titles.title_id
-JOIN publishers
-	ON titles.pub_id = publishers.pub_id
-JOIN sales
-	ON titles.title_id = sales.title_id
-GROUP BY authors.au_id, authors.au_lname, authors.au_fname
-ORDER BY (SUM(sales.qty)) DESC
-LIMIT 3;
+/*Query 2*/
+SELECT film.title AS 'Long Movie', film.length AS 'Duration'
+FROM film
+WHERE film.length > (SELECT AVG(film.length) FROM film)
+ORDER BY length DESC, film.title;
 
 
-/*Challenge 4*/
-SELECT 
-	authors.au_id AS 'AUTHOR ID', 
-    authors.au_lname AS 'LAST NAME', 
-    authors.au_fname AS 'FIRST NAME', 
-	COALESCE(SUM(sales.qty), 0) AS 'TOTAL'
-FROM authors
-LEFT JOIN titleauthor
-	ON authors.au_id = titleauthor.au_id
-LEFT JOIN  titles
-	ON titleauthor.title_id = titles.title_id
-LEFT JOIN publishers
-	ON titles.pub_id = publishers.pub_id
-LEFT JOIN sales
-	ON titles.title_id = sales.title_id
-GROUP BY authors.au_id, authors.au_lname, authors.au_fname
-ORDER BY COALESCE(SUM(sales.qty), 0) DESC;
+/*Query 3*/
+SELECT actor.first_name AS 'First Name', actor.last_name AS 'Last Name'
+FROM actor
+JOIN film_actor 
+	ON film_actor.actor_id = actor.actor_id
+WHERE film_actor.film_id = (
+	SELECT film.film_id
+	FROM film
+	WHERE film.title = 'Alone Trip');
+
+
+/*Query 4*/
+SELECT title AS 'Family Movie', description AS 'Movie Description', rating AS 'MPAA Rating'
+FROM film
+WHERE rating = 'G' OR rating = 'PG';
+
+
+/*Query 5*/
+SELECT address.city_id, country.country, customer.first_name, customer.last_name, customer.email
+FROM address
+LEFT JOIN city
+	ON address.city_id = city.city_id
+LEFT JOIN country
+	ON city.country_id = country.country_id
+LEFT JOIN customer
+	ON address.address_id = customer.address_id
+WHERE country = 'Canada'
+ORDER BY customer.first_name ASC;
+
+
+/*Query 6: find the most prolific Actor*/
+SELECT actor.actor_id, actor.first_name AS 'First Name', actor.last_name AS 'Last Name', COUNT(actor.actor_id) AS 'Total Movie'
+FROM actor
+LEFT JOIN film_actor
+	ON actor.actor_id = film_actor.actor_id
+LEFT JOIN film
+	ON film_actor.film_id = film.film_id
+GROUP BY actor.actor_id, actor.first_name, actor.last_name
+ORDER BY COUNT(actor.actor_id) DESC
+LIMIT 1;
+
+/*Query 6: List of the Movies with the most prolofic Actor in them*/
+SELECT  film.title AS 'Movie starring Gina Degeneres'
+FROM film
+WHERE film.film_id IN (SELECT film_id FROM film_actor WHERE actor_id = 107);
+
+
+/*Query 7: find the most profitable Customer*/
+SELECT customer_id, SUM(amount) AS 'Total spend'
+FROM payment
+GROUP BY customer_id
+ORDER BY SUM(amount) DESC;
+
+
+/*Query 7: find the films rented by the most profitable Customer*/
+SELECT DISTINCT inventory.film_id
+FROM payment
+LEFT JOIN rental
+	ON payment.customer_id = rental.customer_id
+LEFT JOIN inventory
+	ON rental.inventory_id = inventory.inventory_id
+WHERE payment.customer_id = (SELECT customer_id
+FROM payment
+GROUP BY customer_id
+ORDER BY SUM(amount) DESC
+LIMIT 1)
+ORDER BY film_id;
+
+
+/*Query 8*/
